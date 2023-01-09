d2ec06e75d8e871b4f6728533a7f426784867b56
Barbora Herynková <101675313+baraHer@users.noreply.github.com>
2023-01-09 11:39:53 +0100

    Update script.js
    funkce pro ovladani lekci (dalsi/predchozi, close)

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const obsahLekceElement = document.querySelectorAll(".lekce-obsah");
+const oknoLekce = document.querySelector(".lekce-okno")
+
+const tlacitkoPredchozi = document.querySelector(".button-prev")
+const tlacitkoDalsi = document.querySelector(".button-next")
+let cisloZobrazeneLekce
+
 const zobrazOkno = (cisloDne) => {
-  for (let i=0; i < obsahLekceElement.length; i++) {
-    obsahLekceElement[i].style.display = "none";
-  }
-  let indexDne = cisloDne - 1;
-  obsahLekceElement[indexDne].style.display = "block";
-  document.querySelector(".lekce-okno").style.display = "block";
+    for (let i=0; i < obsahLekceElement.length; i++) {
+        obsahLekceElement[i].style.display = "none";
+    }
+    let indexDne = cisloDne - 1;
+    obsahLekceElement[indexDne].style.display = "block";
+    tlacitkoPredchozi.style.display = "block"
+    tlacitkoDalsi.style.display = "block"
+    if (cisloDne === 1) {
+        tlacitkoPredchozi.style.display = "none"
+    }
+    else if (cisloDne === 24) {
+        tlacitkoDalsi.style.display = "none"
+    }
+
+    oknoLekce.style.display = "block";
+    cisloZobrazeneLekce = cisloDne
+}
+
+const zobrazNasledujiciLekci = (soucasnaLekce) => {
+    zobrazOkno(soucasnaLekce + 1)
+}
+
+const zobrazPredchoziLekci = (soucasnaLekce) => {
+    zobrazOkno(soucasnaLekce - 1)
+}
+
+const zavriOkno = () => {
+    oknoLekce.style.display = "none";
 }
